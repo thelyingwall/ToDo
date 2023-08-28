da4f947884e2a3431a51a50d294d015295155e0d
Ala <alarojek1@gmail.com>
2023-08-29 01:32:32 +0200

    validacja ilości znaków, zmiany wizualne (ikona wykrzyknika przy zbliżającym się deadlinie)

--- Models/TaskList.cs
+++ Models/TaskList.cs
@@ -1,8 +1,11 @@
-﻿namespace ToDo.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDo.Models
 {
     public class TaskList
     {
         public int TaskListId { get; set; }
+        [MaxLength(30, ErrorMessage = "Maksymalna ilość znaków wynosi {1}")]
         public string Title { get; set; }
         public string? Description { get; set; }
         public DateTime CreateDate { get; set; }
